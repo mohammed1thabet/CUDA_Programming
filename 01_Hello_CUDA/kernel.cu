99caa8849432f90878da95730d608ecf14e6e903
Mohammed Thabet <43917031+mohammed1thabet@users.noreply.github.com>
2025-01-17 19:36:43 +0200

    add 01_Hello_CUDA example

---
+++ 01_Hello_CUDA/kernel.cu
@@ -0,0 +1,28 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+/*device code(kernel), executed on GPU*/
+__global__ void HelloCuda()
+{
+	printf("Hello CUDA, blockIdx : %d, threadIdx : %2d\n", blockIdx.x, threadIdx.x);
+}
+
+/*host code, executed on CPU*/
+int main()
+{
+
+	/*kernel launch parameters*/
+	dim3 block(32);//block contains 32 threads
+	dim3 grid(2);//grid contains 2 thread blocks
+
+	/*launch kernel to print "Hello CUDA" on 32 threads * 2 blocks in parallel*/
+	HelloCuda << <grid, block >> > ();
+
+	/*wait untill previous kernel execution is completed*/
+	cudaDeviceSynchronize();
+	cudaDeviceReset();
+	return 0;
+}
